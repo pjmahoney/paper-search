55cbcbc73f136ab9f627fdd6af3b6b43008ae267
Patrick Mahoney <patrick@sitesee.io>
2018-06-21 11:52:53 +1000

    Initial polymer3 update - modularizer untested

---
+++ paper-search.js
@@ -0,0 +1,10 @@
+import './paper-search-bar.js';
+import './paper-filter-dialog.js';
+import './paper-search-panel.js';
+
+/*
+Workaround for https://github.com/PolymerElements/iron-component-page/issues/34
+
+TODO: Remove this file once the ticket is implemented.
+*/
+;
